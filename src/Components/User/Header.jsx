e7a1d36ffa929697b5222cabf8b060203f5fa376
slovetechnology <slovetech619@gmail.com>
2024-12-01 08:33:06 +0100

    new updates

--- src/Components/User/Header.jsx
+++ src/Components/User/Header.jsx
@@ -5,7 +5,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import Lottie from 'react-lottie';
 import animationData from '../../Lotties/task-colony';
 import { SlBell, SlMagnifier, SlMenu } from 'react-icons/sl';
-import Navbar from '../General/Navbar';
 import { useSelector } from 'react-redux';
 import Cookies from 'js-cookie';
 import { NavLinks, TopNavsLinks } from '../../utils/utils';
@@ -15,6 +14,8 @@ const Header = () => {
   const navigate = useNavigate();
   const [mobile, setMobile] = useState(false)
   const MobileIcon = mobile ? FaTimes : SlMenu
+  const [topNav, setTopNav] = useState(false)
+  const TopNavIcon = topNav ? FaTimes : SlMenu
 
   const defaultOptions = {
     loop: true,
@@ -23,12 +24,6 @@ const Header = () => {
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice',
     },
-  };
-
-  const [view, setView] = useState(false);
-
-  const toggleView = () => {
-    setView(prevView => !prevView);
   };
 
   const handleLogout = () => {
@@ -40,74 +35,86 @@ const Header = () => {
 
   return (
     <div>
-      {view && <Navbar closeView={toggleView} />}
+      <div className={`fixed w-full bg-primary z-50 border-b shadow-xl lg:py-5 py-4 gap-10 ${topNav ? 'h-[20rem]' : 'h-[4.6rem]'} transition-all`}>
+        <div className='flex items-center justify-between w-11/12 lg:w-10/12 mx-auto'>
+          <div className="flex items-center gap-5">
+            <Link to="/" className="hidden lg:block">
+              <Lottie options={defaultOptions} height={40} width={140} />
+            </Link>
+            <Link to="/" className="lg:hidden block">
+              <Lottie options={defaultOptions} height={40} width={125} />
+            </Link>
 
-      <div className="fixed w-full bg-primary z-50 border-b shadow-xl flex items-center lg:py-5 py-4 justify-between px-[10rem] gap-10">
-        <div className="flex items-center gap-5">
-          <Link to="/" className="hidden lg:block">
-            <Lottie options={defaultOptions} height={40} width={140} />
-          </Link>
-          <Link to="/" className="lg:hidden block">
-            <Lottie options={defaultOptions} height={40} width={125} />
-          </Link>
-
-        </div>
-        <div className="lg:flex flex-row items-center gap-4 hidden">
-          <div className="border border-slate-500 w-[15rem] rounded-md p-2 flex items-center gap-1">
-            <input type="text" placeholder='What are you looking for today?' className="outline-none w-full text-xs bg-transparent" />
-            <SlMagnifier className='text-zinc-400 text-xs' />
           </div>
-          {TopNavsLinks.map((item, index) => (
-            <Link to={item.link} key={index} className='uppercase text-xs py-3 px-4 truncate'>{item.title}</Link>
-          ))}
+          <div className="lg:flex flex-row items-center gap-4 hidden">
+            <div className="border border-slate-500 w-[15rem] rounded-md p-2 flex items-center gap-1">
+              <input type="text" placeholder='What are you looking for today?' className="outline-none w-full text-xs bg-transparent" />
+              <SlMagnifier className='text-zinc-400 text-xs' />
+            </div>
+            {TopNavsLinks.map((item, index) => (
+              <Link to={item.link} key={index} className='uppercase text-xs py-3 px-4 truncate'>{item.title}</Link>
+            ))}
+          </div>
+          <div className="flex items-center gap-3">
+            <Link to='/user' className="">
+              {userloggedin ? (
+                <div className="flex items-center gap-4">
+                  <div className="flex items-center gap-2">
+                    <FaUserCircle className="lg:text-[2rem] text-[1rem] bg-gray-200" />
+                    <div className='text-xs hidden xl:block pr-5'>
+                      <p className="mb-0">{user?.firstname || 'User'}</p>
+                      <p className="text-sm font-medium text-secondary-500">{user?.email}</p>
+                    </div>
+                  </div>
+                  <div className="xl:hidden flex lg:text-2xl cursor-pointer">
+                    <TopNavIcon onClick={() => setTopNav(!topNav)} />
+                  </div>
+                  <div className="lg:text-3xl xl:hidden">
+                    <div onClick={handleLogout} className="flex cursor-pointer items-center text-secondary gap-2">
+                      <IoIosLogOut />
+                    </div>
+                  </div>
+
+                  <div className="hidden xl:flex">
+                    <div onClick={handleLogout} className="flex cursor-pointer items-center text-secondary gap-2">
+                      <IoIosLogOut /> Logout
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="">
+                  <div className="hidden xl:flex">
+                    <div
+                      onClick={() => navigate('/login')}
+                      className="bg-secondary flex text-white gap-2 h-11 w-[9.5rem] justify-center rounded-md items-center cursor-pointer"
+                    >
+                      <FaUserPlus /> Login/Register
+                    </div>
+
+                  </div>
+                  <div onClick={() => navigate('/login')} className="block text-3xl mx-5 xl:hidden">
+                    <FaUserCircle />
+                  </div>
+                </div>
+              )}
+            </Link>
+
+            <div className="xl:hidden flex text-xl lg:text-2xl cursor-pointer">
+              <TopNavIcon onClick={() => setTopNav(!topNav)} />
+            </div>
+          </div>
         </div>
-        <div className="flex items-center">
-          <Link to='/user' className="">
-            {userloggedin ? (
-              <div className="flex items-center gap-4">
-                <div className="flex items-center gap-2">
-                  <FaUserCircle className="lg:text-[2rem] text-[1rem] bg-gray-200" />
-                  <div className='text-xs hidden xl:block pr-5'>
-                    <p className="mb-0">{user?.firstname || 'User'}</p>
-                    <p className="text-sm font-medium text-secondary-500">{user?.email}</p>
-                  </div>
-                </div>
-                <div className="xl:hidden flex lg:text-2xl cursor-pointer">
-                  <SlMenu onClick={toggleView} />
-                </div>
-                <div className="lg:text-3xl xl:hidden">
-                  <div onClick={handleLogout} className="flex cursor-pointer items-center text-secondary gap-2">
-                    <IoIosLogOut />
-                  </div>
-                </div>
-
-                <div className="hidden xl:flex">
-                  <div onClick={handleLogout} className="flex cursor-pointer items-center text-secondary gap-2">
-                    <IoIosLogOut /> Logout
-                  </div>
-                </div>
-              </div>
-            ) : (
-              <div className="">
-                <div className="hidden xl:flex">
-                  <div
-                    onClick={() => navigate('/login')}
-                    className="bg-secondary flex text-white gap-2 h-11 w-[9.5rem] justify-center rounded-md items-center cursor-pointer"
-                  >
-                    <FaUserPlus /> Login/Register
-                  </div>
-
-                </div>
-                <div onClick={() => navigate('/login')} className="block text-3xl mx-5 xl:hidden">
-                  <FaUserCircle />
-
-                </div>
-              </div>
-            )}
-
-          </Link>
-
-        </div>
+       {topNav && <div className="w-11/12 lg:w-10/12 mx-auto mt-6">
+          <div className="flex flex-col">
+            <div className="border mb-3 border-slate-500 w-full rounded-md p-4 flex items-center gap-1">
+              <input type="text" placeholder='What are you looking for today?' className="outline-none w-full text-xs bg-transparent" />
+              <SlMagnifier className='text-zinc-400 text-xs' />
+            </div>
+            {TopNavsLinks.map((item, index) => (
+              <Link to={item.link} key={index} className='uppercase text-xs py-3 px-4 truncate'>{item.title}</Link>
+            ))}
+          </div>
+        </div>}
       </div>
       <br />
       <br />
@@ -120,9 +127,9 @@ const Header = () => {
           {NavLinks.map((item, index) => (
             <Link to={`${item.link}`} key={index} className='text-xs hover:text-white transition-all truncate uppercase text-orange-100 py-3 px-3'>{item.title}</Link>
           ))}
-         {mobile ?
-         <Link to={``} className='text-xs truncate uppercase hover:text-white transition-all text-orange-100 py-3 px-3'>notifications</Link>
-         :  <Link to="" className='text-white relative text-sm'> <SlBell /> <div className="absolute -top-2 -right-2 bg-black text-white flex items-center justify-center size-4 rounded-full text-[0.7rem]">1</div> </Link>}
+          {mobile ?
+            <Link to={``} className='text-xs truncate uppercase hover:text-white transition-all text-orange-100 py-3 px-3'>notifications</Link>
+            : <Link to="" className='text-white relative text-sm'> <SlBell /> <div className="absolute -top-2 -right-2 bg-black text-white flex items-center justify-center size-4 rounded-full text-[0.7rem]">1</div> </Link>}
         </div>
       </div>
     </div>
