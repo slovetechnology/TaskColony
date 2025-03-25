01aba6c0dea20d6593276f57215f9dfd807d5f01
olanrewajudev <olagitdev@gmail.com>
2025-03-25 22:54:23 +0100

    new update

--- src/Pages/General/Notification.jsx
+++ src/Pages/General/Notification.jsx
@@ -42,7 +42,7 @@ const Notification = ({ closeview }) => {
     <Modal height='h-[40rem]' closeView={closeview}>
       <div className="font-bold text-2xl mb-4">Notification</div>
       <div className="flex justify-end">
-        {/* <button className="bg-secondary px-4 py-2 text-white rounded-lg" onClick={Markasread()}>Mark As Read</button> */}
+        <button className="bg-secondary px-4 py-2 text-white rounded-lg" onClick={Markasread()}>Mark As Read</button>
       </div>
       <div className="mt-6">
         <div>
